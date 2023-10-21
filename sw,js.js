09b22bd0f273933487b9081a8446f6397d44f0df
mk <43450424+aloproducao@users.noreply.github.com>
2023-10-21 01:29:12 -0300

    pwa

---
+++ sw,js.js
@@ -0,0 +1,34 @@
+// Definindo os arquivos para armazenar em cache
+const CACHE_NAME = 'voz-livre-cache-v1';
+const urlsToCache = [
+  '/',
+  'styles.css',
+  'script.js',
+  'logo.png',
+  'https://cdn.jsdelivr.net/npm/daisyui@3.9.3/dist/full.css',
+  'https://cdn.tailwindcss.com'
+];
+
+// Instalação do Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch para responder as requisições com os arquivos em cache quando disponíveis
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
